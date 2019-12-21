71f38e2bbb27d38efb111d82e9b593d93c9753c3
janosimas <janosimas@gmail.com>
2019-12-21 15:07:27 +0100

    ETL
    declarative implementation

--- etl/src/lib.rs
+++ etl/src/lib.rs
@@ -1,11 +1,11 @@
 use std::collections::BTreeMap;
 
 pub fn transform(h: &BTreeMap<i32, Vec<char>>) -> BTreeMap<char, i32> {
-    let mut r: BTreeMap<char, i32> = BTreeMap::new();
-    h.iter().for_each(|(value, list)| {
-        list.iter().for_each(|c| {
-            r.insert(c.to_ascii_lowercase(), *value);
-        });
-    });
-    r
+    h.iter()
+        .flat_map(|(val, list)| {
+            list.iter()
+                .map(char::to_ascii_lowercase)
+                .zip(std::iter::repeat(*val))
+        })
+        .collect::<BTreeMap<char, i32>>()
 }

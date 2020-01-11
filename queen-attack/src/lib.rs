3abc9cd665a49edd0ad096cfd6dfe0399517cb15
janosimas <janosimas@gmail.com>
2020-01-11 14:58:37 +0100

    queen-attack

---
+++ queen-attack/src/lib.rs
@@ -0,0 +1,29 @@
+#[derive(Debug)]
+pub struct ChessPosition(i32, i32);
+
+#[derive(Debug)]
+pub struct Queen {
+    position: ChessPosition,
+}
+
+impl ChessPosition {
+    pub fn new(rank: i32, file: i32) -> Option<Self> {
+        if !(0..8).contains(&rank) || !(0..8).contains(&file) {
+            return None;
+        }
+        Some(ChessPosition(rank, file))
+    }
+}
+
+impl Queen {
+    pub fn new(position: ChessPosition) -> Self {
+        Queen { position }
+    }
+
+    pub fn can_attack(&self, other: &Queen) -> bool {
+        self.position.0 == other.position.0
+            || self.position.1 == other.position.1
+            || (self.position.0 + self.position.1 == other.position.0 + other.position.1)
+            || (self.position.0 - other.position.0 == self.position.1 - other.position.1)
+    }
+}
